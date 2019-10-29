e50dca7a995e331f950e5bc117322056b676f5bf
NicolasGomez <sala@HOR-SALA501-015.unihorizonte.salas>
2019-10-29 15:54:15 -0500

    MI PRIMER COMMIT

---
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -0,0 +1,22 @@
+
+package senasalud;
+
+import java.util.Scanner;
+
+
+public class SenaSalud {
+
+   
+    public static void main(String[] args) {
+        String nom;
+  Scanner nombre =new Scanner (System.in);
+  System.out.println("Digite su Nombre");
+  nom=nombre.next();
+  System.out.println("Su nombre es:"+nom);
+                
+              
+  
+    }
+    
+}
+
